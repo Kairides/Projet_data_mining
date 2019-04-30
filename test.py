2cd3703bd5a8716fa4c77146d3306dca038f04db
E16B498D <charles.bequet--ermoy@etu.univ-nantes.fr>
2019-04-30 13:59:18 +0200

    petite amélioration

--- test.py
+++ test.py
@@ -22,10 +22,7 @@ for i in range(0, tracks.index[-1]+1):
 
     tracks.at[i, 'pos_pic'] = min(track_dict[tracks.at[i, 'url']])
 
-    if min(track_dict[tracks.at[i, 'url']]) < 15 :
-        tracks.at[i, 'pic15'] = 1
-    else:
-        tracks.at[i, 'pic15'] = 0
+    tracks.at[i, 'pic15'] = int(min(track_dict[tracks.at[i, 'url']]) < 15)
     
     livespan = len(track_dict[tracks.at[i, 'url']])
     tracks.at[i, 'livespan'] = livespan
@@ -33,9 +30,6 @@ for i in range(0, tracks.index[-1]+1):
     position_moyenne = sum(track_dict[tracks.at[i, 'url']])/livespan
     tracks.at[i, 'pos_avg'] = position_moyenne
 
-    if position_moyenne < 15:
-        tracks.at[i, 'avg15'] = 1
-    else:
-        tracks.at[i, 'avg15'] = 0
+    tracks.at[i, 'avg15'] = int(position_moyenne < 15)
 
 print(tracks)
